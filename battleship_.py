7eacdcb1b7badb381351ae518a2bf3860b56c053
TokamLilian <tokamlilian@gmail.com>
2023-04-02 05:40:31 -0400

    Ajouter les derniers commentaires et nettoyer le code

--- battleship_.py
+++ battleship_.py
@@ -27,7 +27,7 @@ def arc(r, angle):
 
 
 def cercle(r):
-#Cette fonction permet de dessiner un cercle
+#Cette fonction permet de dessiner un cercle rouge qui représente un bateau
 
     turtle.color("red")
     arc(r, 360)
@@ -67,28 +67,29 @@ def creer_joueur(nom):
 def shuffle(joueur, cols):                                       
 #cette fonction écrit les positions aléatoires des bateaux d'un joueur
 
-    while len(joueur["bateaux"]) < 2:  ##                                            #chaque joueur a 5 bateaux
+    while len(joueur["bateaux"]) < cols-1:                                        #chaque joueur a 5 bateaux
         ligne = int(cols*random.random()) + 1                                     #la position aleatoire d'un bateau
         colonne = int(cols*random.random())
         position = lettres_collones[colonne] + str(ligne)
 
-        if position not in joueur["bateaux"]:                                    #on veut s'assurer que tous les bateaux sont distincts
+        if position not in joueur["bateaux"]:                                     #on veut s'assurer que tous les bateaux sont distincts
             joueur["bateaux"].append(position)
 
 
 def gagnant (joueurs):
+#cette fontion retourne le meilleur joueur et son nombre de points
         
-        meilleur_joueur = joueurs[0]
+        meilleur_joueur = joueurs[0]                                              #le meilleur joueur est le joueur 1 par défaut
         point_max = meilleur_joueur["points"]
 
-        for i in range (len(joueurs)):
-            joueur = joueurs[i]
-            point = joueur["points"]
+        for i in range (len(joueurs)):                                            #on parcoure les points de différents joueurs et
+            joueur = joueurs[i]                                                   #si on trouve un joueur qui a plus de point, alors
+            point = joueur["points"]                                              #il est le meilleur joeur
             if point > point_max:
                 point_max = point
                 meilleur_joueur = joueurs[i]["nom"]
 
-        meilleur = [meilleur_joueur["nom"], point_max]
+        meilleur = [meilleur_joueur, point_max]
         return meilleur
 
 
@@ -96,91 +97,42 @@ def get_coordinates(cible ,cols):
 #cette fontion retourne les coordonées d'une cible proposé
 
     letter = cible[0]
-    number = int(cible[1])
+    number = int(cible[1]) - 1
 
-    position = 0
+    colonne = 0
 
-    if letter not in lettres_collones or number > 6:
-        return
+    if letter not in lettres_collones or number > 5 or number < 0:                #on veut s'assurer que les entrées de l'utilisateur correspondent 
+        return                                                                    #aux cases du jeux, sinon on retoune demander une nouvelle entrée
 
-    for letters in lettres_collones:
-        if letter == letters:
-            column = position
-            break
-        else:
-            position += 1
+    for letters in lettres_collones:                                              #on parcourure les lettres des colonnes pour identifier la position
+                                                                                  #de la la lettre dès qu'on la trouve
+        if letter == letters: break                             
 
-    line = number - 1
+        else: colonne += 1                                                        #on passe à la prochaine colonne si on a pas encore trouvé la lettre
 
-    ligne_i = line
-    ligne_x = 0
-    d = 1
-    while True:
-        if ligne_i == (cols - d):
-            break
-        else:
-            d += 1
-            ligne_x += 1
+    ligne = 0                                                                     #la grille étant déssinée de bas vers le haut, on veut avoir la ligne
+                                                                                  #qui correspond à la case relativement à la grille 
+    for pas in range(1, cols):        
 
-    coordinate = [ligne_x, column]
+        if number == (cols - pas):  break                                         #on remonte d'une ligne si on n'est pas à au numero de l'entrée
 
-    return(coordinate)
+        else:   ligne += 1
 
+    coordinate = [ligne, colonne]                                                 #les coordonnées le la cible sur la grille déssinée par la tortue
 
-def prochaine_partie_rate(joueur, taille, espace, cols):                 
-#cette fontion dessine les cases ratées
-
-    for rate in joueur["rates"]:                                                    #pour toutes les coordonnées des propositions ratés
-    #on veut aller à la grille correspondante et dessiner un raté
-
-        ligne_i = rate[0]
-        colonne_i = rate[1]
-
-        ligne_x = 0
-        d = 1
-        while True:
-            if ligne_i == (cols - d):
-                break
-            else:
-                d += 1
-                ligne_x += 1
-
-        positionner(colonne_i * (taille + espace), ligne_x * (taille + espace))
-        dessiner_rate(taille)
-        positionner(-(colonne_i  * (taille + espace) + taille), -ligne_x * (taille + espace))
-
-
-def prochaine_partie_touche(joueur, taille, espace, cols):                 
-#cette fontion dessine les cases touchées
-
-    for touch in joueur["trouves"]:                                                                 #pour toutes les coordonnées des cibles touchés
-    #on veut aller à la grille correspondante et dessiner un bateau
-        
-        ligne_i = touch[0]
-        colonne_i = touch[1]
-
-        colonne_x = colonne_i - 1
-        ligne_x = 0
-        d = 1
-        while True:
-            if ligne_i == (cols - d):
-                break
-            else:
-                d += 1
-                ligne_x += 1
-
-        positionner(colonne_i * (taille + espace) + taille/2, ligne_x * (taille + espace))          #on va au milieu du carré pour pouvoir dessiner le cercle
-        cercle(8)                                                                                   #dessiner un cercle de rayon 8
-        positionner(-colonne_i * (taille + espace) - taille/2, -ligne_x * (taille + espace))        #on va au milieu du carré pour pouvoir dessiner le cercle
+    return coordinate
 
 
 def dessiner_rate(taille):
+#cette fonction dessine une case ratée représentée par un carré vert avec une croix à l'intérieure
+
     turtle.color("green")
     turtle.pensize(2)
     carre(taille)
 
     hypothenus = taille*math.sqrt(2)
     lt(45);fd(hypothenus);pu();lt(135);fd(taille);pd();lt(135);fd(hypothenus);lt(45)
+    fd(-taille)
     turtle.color("black")
     turtle.pensize(1)
 
@@ -193,28 +145,30 @@ def grille (joueur, cols, lignes, taille, espace):
             coordinate = [y, x]
 
 
-            if coordinate in joueur["rates"]:
+            if coordinate in joueur["rates"]:                                     #pour dessiner toutes les cases ratées
                 positionner(x * (taille + espace), y * (taille + espace))  
                 dessiner_rate(taille)
-                positionner(-(x * (2*taille + espace)), -y * (taille + espace))
+                positionner(-(x * (taille + espace)), -y * (taille + espace))
 
-            elif coordinate in joueur["trouves"]:
+            elif coordinate in joueur["trouves"]:                                 #pour dessiner toutes les cases trouvées
                 positionner(x * (taille + espace) + taille/2, y * (taille + espace))
                 cercle(taille/2)
                 positionner(-x * (taille + espace) - taille/2, -y * (taille + espace))
 
-            else:
+            else:                                                                 #pour dessiner toutes les autres cases
                 positionner(x * (taille + espace), y * (taille + espace))
                 carre(taille)
                 positionner(-x * (taille + espace), -y * (taille + espace))
 
 
 def dessiner_ligne(hauteur, largueur):
+#cette fonction dessine une ligne séparatrice entre les deux grilles du jeux
+
     size = [largueur, hauteur]
 
     turtle.color("blue")
- 
     turtle.begin_fill()
+
     for _ in range (2):
         for move in size:
             fd(move)
@@ -225,17 +179,18 @@ def dessiner_ligne(hauteur, largueur):
 
     
 def dessins(joueurs, taille, espace, cols, lignes, largueur, distance):
+#cette fonction fait appel aux dessins des grilles du jeux ainsi que la ligne entre elles
 
-    hauteur = (lignes * taille) + ((lignes - 1) * espace)               #la hauteur de la ligne separatrice
+    hauteur = lignes * taille + 2*espace*(lignes-1)                         #la hauteur de la ligne separatrice
     
-    step = (cols * taille) + ((cols - 1) * espace) + distance           #le pas pour aller à la dernière case
+    step = (cols * taille) + ((cols - 1) * espace) + distance               #le pas pour aller à la dernière case
 
     for i in range(len(joueurs)):
         joueur = joueurs[i]
         grille(joueur, cols, lignes, taille, espace)
 
-        if i == 0:
-            pu()
+        if i == 0:                                                          #on ne dessine la ligne que après la première
+            pu()                                                            #grille pour ce jeux
             positionner(step, 0)
             pd()
             dessiner_ligne(hauteur, largueur)
@@ -243,26 +198,28 @@ def dessins(joueurs, taille, espace, cols, lignes, largueur, distance):
 
 
 def jouer():
+#cette fonction est la fonction principale du jeux qui initialise la partie et procède à l'appel d'autres fontions
+
     nombre_joueurs = 2
-    joueurs = []
+    joueurs = []                                            #la liste des joueurs du jeux
     cols = 6
     lignes = 6
 
     distance = 20                                           #la distance entre la ligne bleu et une grille
-    largueur = 10
+    largueur = 10                                           #la largueur de la ligne séparatrice
 
     taille = 16                                             #la taille d'un carré
     espace = 4                                              #l'espace entre deux carrés
 
-    cont = True
+    cont = True                                             #la decision de continuer le jeux ou pas
 
     for i in range(nombre_joueurs):                          #on creait les joueurs du jexu avec leurs bateaux
         joueurs.append(creer_joueur(f"Joueur {i + 1}"))
 
         shuffle(joueurs[i], cols)
-        print(joueurs[i]) ##
 
-    turtle.hideturtle()
+    #turtle.shape("arrow")                                   #si on veut changer le type de figure de la tortue
+    #turtle.hideturtle()                                     #si on decide de cacher la tortue
     dessins(joueurs, taille, espace, cols, lignes, largueur, distance)
 
     while cont:
@@ -271,20 +228,22 @@ def jouer():
 
             if joueur == 1: 
                 autre = 0
-                pu(); goto(0,0); pd()  
+                pu(); goto(0,0); pd()                        #on positionne la tortue à au début de la grille de l'adversaire 
 
             else: 
                 autre = 1
+                #on calcul le nombre de pas pour aller aux pieds de la grille du joueur adverse
                 steps = (taille * cols) + (cols-1)*espace + largueur + 2*distance
                 pu(); goto (steps, 0); pd() 
 
-            print('Joueur',joueur+1)
+            print(joueurs[joueur]["nom"])
 
             while True:
                 proposition = input('Visez un bateau adverse: ')
-                proposition = proposition.upper()
-                cible = get_coordinates(proposition, cols)
-                if cible != None: break
+                proposition = proposition.upper().strip()
+
+                cible = get_coordinates(proposition, cols)                  #si un joueur entre une cible non valid, le programme lui demandera
+                if cible != None: break                                     #d'entrer une cible à nouveau
 
             if cible in joueurs[autre]["trouves"]:
                 print('Vous avez déjà touché ce bateau'); print("")
@@ -293,7 +252,7 @@ def jouer():
 
                 print("Touché!"); print("")
                 joueurs[autre]["trouves"].append(cible)                     #les bateaux touchés par l'adversaire
-                joueurs[joueur]["points"] += 1                              #on donne les points du joeuur actuel                                           
+                joueurs[joueur]["points"] += 1                              #on donne les points du joueur actuel                                           
 
             else:
                 joueurs[autre]["rates"].append(cible)
@@ -302,8 +261,7 @@ def jouer():
             pu()
             goto(0,0)
             turtle.clear()
-            dessins(joueurs, taille, espace,cols, lignes, largueur, distance)
-                ##prochaine_partie_rate(joueurs[joueur], taille, espace, cols)
+            dessins(joueurs, taille, espace,cols, lignes, largueur, distance)          #on redessine la grille avec les cases touchées et/ou ratées
 
             nombre_touches_0 = joueurs[0]["trouves"]
             nombre_touches_1 = joueurs[1]["trouves"]
@@ -318,7 +276,7 @@ def jouer():
 
     print('Le meilleur joueur est',meilleur_jouer,'avec',meilleur_point,'points'); print("")
 
-    print('Clickez n\'importe où pour terminer')
+    print('Clickez sur une grille pour terminer')
     turtle.exitonclick()
 
 
